c89471d84754dc373bc5e39dcc2bcd5f7f9adf93
sasori7611san <love.ayap.sayap.0426@star4.gmobb.jp>
2022-09-22 17:53:41 +0900

    不要コメント削除

--- panel-system/src/App.vue
+++ panel-system/src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <PanelSystem />
-  <!-- <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div> -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-// import HelloWorld from './components/HelloWorld.vue'
 import PanelSystem from './components/PanelSystem.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    // HelloWorld,
     PanelSystem
   }
 })
