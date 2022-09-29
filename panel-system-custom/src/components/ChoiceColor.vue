55a3d294fd6d1e2116e09944fe6843adbb8f5be5
sasori7611san <love.ayap.sayap.0426@star4.gmobb.jp>
2022-09-29 17:17:00 +0900

    component分割修正

--- panel-system-custom/src/components/ChoiceColor.vue
+++ panel-system-custom/src/components/ChoiceColor.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="choice">
-      <button id="buttonRed" @click="choiceColor?.(2)">{{ redCount }}</button>
-      <button id="buttonGreen" @click="choiceColor?.(3)">{{ greenCount }}</button>
-      <button id="buttonWhite" @click="choiceColor?.(4)">{{ whiteCount }}</button>
-      <button id="buttonBlue" @click="choiceColor?.(5)">{{ blueCount }}</button><br />
-      <button id="buttonYellow" @click="choiceColor?.(1)"></button>
+      <button id="buttonRed" @click="choiceRed()">{{ redCount }}</button>
+      <button id="buttonGreen" @click="choiceGreen()">{{ greenCount }}</button>
+      <button id="buttonWhite" @click="choiceWhite()">{{ whiteCount }}</button>
+      <button id="buttonBlue" @click="choiceBlue()">{{ blueCount }}</button><br />
+      <button id="buttonYellow" @click="choiceYellow()"></button>
     </div>
     <p>更新で最初からに戻ります。</p>
   </div>
@@ -27,7 +27,32 @@ export default defineComponent({
     const greenCount = toRef(props, 'greenCountRef')
     const whiteCount = toRef(props, 'whiteCountRef')
     const blueCount = toRef(props, 'blueCountRef')
-    return { redCount, greenCount, whiteCount, blueCount }
+    const choiceRed = () => {
+      if (props.choiceColor) {
+        props.choiceColor(2)
+      }
+    }
+    const choiceGreen = () => {
+      if (props.choiceColor) {
+        props.choiceColor(3)
+      }
+    }
+    const choiceWhite = () => {
+      if (props.choiceColor) {
+        props.choiceColor(4)
+      }
+    }
+    const choiceBlue = () => {
+      if (props.choiceColor) {
+        props.choiceColor(5)
+      }
+    }
+    const choiceYellow = () => {
+      if (props.choiceColor) {
+        props.choiceColor(1)
+      }
+    }
+    return { redCount, greenCount, whiteCount, blueCount, choiceRed, choiceGreen, choiceWhite, choiceBlue, choiceYellow }
   }
 })
 </script>
