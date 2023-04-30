b4040471cd598f8ff09299fd4342217e0c49b95e
sasori7611san <107899079+sasori7611san@users.noreply.github.com>
2023-04-30 13:19:00 +0900

    誤記修正とenums修正

--- panel-system-custom/src/components/modules/panelChangeExec.ts
+++ panel-system-custom/src/components/modules/panelChangeExec.ts
@@ -14,7 +14,7 @@ let sheets = 0
 // パネル更新
 // 時間差をつけて更新させるためパネル変化数を引数から取得し、各方向で対象となるパネルに対して更新処理を行う
 // パネル更新（上方向）（col:対象色番号,v:縦番号,s:横番号,pan:パネル,total:パネル集計,sheet:パネル変化枚数）
-export const upPanelChenge = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
+export const upPanelChange = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
   sheets = sheet
   for (i = v - 1; i >= 0; i--) {
     if (pan[i][s].colorNo !== col && pan[i][s].colorNo > COLORS.YELLOW) {
@@ -26,7 +26,7 @@ export const upPanelChenge = (col: number, v: number, s: number, pan: Panel[][],
   return sheets
 }
 // パネル更新（下方向）（col:対象色番号,v:縦番号,s:横番号,pan:パネル,total:パネル集計,sheet:パネル変化枚数）
-export const downPanelChenge = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
+export const downPanelChange = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
   sheets = sheet
   for (i = v + 1; i <= 6; i++) {
     if (pan[i][s].colorNo !== col && pan[i][s].colorNo > COLORS.YELLOW) {
@@ -38,7 +38,7 @@ export const downPanelChenge = (col: number, v: number, s: number, pan: Panel[][
   return sheets
 }
 // パネル更新（左方向）（col:対象色番号,v:縦番号,s:横番号,pan:パネル,total:パネル集計,sheet:パネル変化枚数）
-export const leftPanelChenge = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
+export const leftPanelChange = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
   sheets = sheet
   for (i = s - 1; i >= 0; i--) {
     if (pan[v][i].colorNo !== col && pan[v][i].colorNo > COLORS.YELLOW) {
@@ -50,7 +50,7 @@ export const leftPanelChenge = (col: number, v: number, s: number, pan: Panel[][
   return sheets
 }
 // パネル更新（右方向）（col:対象色番号,v:縦番号,s:横番号,pan:パネル,total:パネル集計,sheet:パネル変化枚数）
-export const rightPanelChenge = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
+export const rightPanelChange = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
   sheets = sheet
   for (i = s + 1; i <= 6; i++) {
     if (pan[v][i].colorNo !== col && pan[v][i].colorNo > COLORS.YELLOW) {
@@ -62,7 +62,7 @@ export const rightPanelChenge = (col: number, v: number, s: number, pan: Panel[]
   return sheets
 }
 // パネル更新（左斜め上方向）（col:対象色番号,v:縦番号,s:横番号,pan:パネル,total:パネル集計,sheet:パネル変化枚数）
-export const leftUpPanelChenge = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
+export const leftUpPanelChange = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
   sheets = sheet
   for (i = v - 1, j = s - 1; i >= 0 || j >= 0; i--, j--) {
     if (pan[i][j].colorNo !== col && pan[i][j].colorNo > COLORS.YELLOW) {
@@ -74,7 +74,7 @@ export const leftUpPanelChenge = (col: number, v: number, s: number, pan: Panel[
   return sheets
 }
 // パネル更新（左斜め下方向）（col:対象色番号,v:縦番号,s:横番号,pan:パネル,total:パネル集計,sheet:パネル変化枚数）
-export const leftDownPanelChenge = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
+export const leftDownPanelChange = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
   sheets = sheet
   for (i = v + 1, j = s - 1; i <= 6 || j >= 0; i++, j--) {
     if (pan[i][j].colorNo !== col && pan[i][j].colorNo > COLORS.YELLOW) {
@@ -86,7 +86,7 @@ export const leftDownPanelChenge = (col: number, v: number, s: number, pan: Pane
   return sheets
 }
 // パネル更新（右斜め上方向）（col:対象色番号,v:縦番号,s:横番号,pan:パネル,total:パネル集計,sheet:パネル変化枚数）
-export const rightUpPanelChenge = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
+export const rightUpPanelChange = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
   sheets = sheet
   for (i = v - 1, j = s + 1; i >= 0 || j <= 6; i--, j++) {
     if (pan[i][j].colorNo !== col && pan[i][j].colorNo > COLORS.YELLOW) {
@@ -98,7 +98,7 @@ export const rightUpPanelChenge = (col: number, v: number, s: number, pan: Panel
   return sheets
 }
 // パネル更新（右斜め下方向）（col:対象色番号,v:縦番号,s:横番号,pan:パネル,total:パネル集計,sheet:パネル変化枚数）
-export const rightDownPanelChenge = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
+export const rightDownPanelChange = (col: number, v: number, s: number, pan: Panel[][], total: Total, sheet: number): number => {
   sheets = sheet
   for (i = v + 1, j = s + 1; i <= 6 || j <= 6; i++, j++) {
     if (pan[i][j].colorNo !== col && pan[i][j].colorNo > COLORS.YELLOW) {
